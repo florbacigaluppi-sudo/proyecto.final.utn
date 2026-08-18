857fd738de60d0d5a63d6c550b48ca06e4d3821c
florencia.bacigaluppi <florbacigaluppi@gmail.com>
2026-08-18 16:19:50 -0300

    add: Title, Textarea and Ckeckboxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
 import { Header } from "./components/Header"
+import { useState } from "react"
 
 
 
 
-function App () {
+const App = () => {
+    const  [text, setText]= useState ("Design is the silent ambassador of your brand. Simplicity is key to effective communication, creating clarity in every interaction. A great design transforms complex ideas into elegant solutions, making them easy to understand. It blends aesthetics and functionality seamlessly.")
+
+    const [excludeSpaces, setExcludeSpaces] = useState(false)
+
+    const [limitCharacter, setLimitCharacter] = useState (false)
+
+const character = excludeSpaces? text.replace(/\s/g,"").lenght : text.lenght
 return <main>
     <Header/>
+    <h2>Analize your text<br />in real-time </h2>
+    <textarea 
+    placeholder= "Ingrese su texto"
+    onChange= {(e) => setText (e.target.value)}
+    value={text}
+     ></textarea>
+     <div>
+        <label>
+        <input 
+        type="checkbox"
+        checked= {excludeSpaces}
+        onChange={()=>setExcludeSpaces(!excludeSpaces)}></input>
+        Exclude Spaces</label>
+
+         <label>
+        <input 
+        type="checkbox"
+        checked= {limitCharacter}
+        onChange={()=> setLimitCharacter (!limitCharacter)}></input>
+        Set Character Limit</label>
+        </div>
+      
+
 </main>
 }
 
